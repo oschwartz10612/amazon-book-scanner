1e29e8ea5df8ec7a61495ab66e279db0fc42ad41
Owen Schwartz <oschwartz10612@gmail.com>
2021-04-02 18:53:01 -0400

    Working

--- app.js
+++ app.js
@@ -1,48 +1,126 @@
 require('dotenv').config()
 const SellingPartnerAPI = require('amazon-sp-api');
+const prompt = require('prompt-validate');
+var player = require('play-sound')(opts = {})
 
 let sellingPartner = new SellingPartnerAPI({
-    region:'na', // The region of the selling partner API endpoint ("eu", "na" or "fe")
+    region: 'na', // The region of the selling partner API endpoint ("eu", "na" or "fe")
     refresh_token: process.env.REFRESH_TOKEN // The refresh token of your app user
 });
 
+async function main() {
+    const ISBN = prompt('ISBN >> ');
 
+    if (ISBN == 'stop') {
+        process.exit();
+    }
 
-// (async() => {
-//     try {
-//       let sellingPartner = new SellingPartnerAPI({
-//         region:'na', // The region of the selling partner API endpoint ("eu", "na" or "fe")
-//         refresh_token:'Atzr|IwEBIC4AJDFKXvRUub4yuIarsXbxYdBhZNCBWrr3hIOBoOlUcHvOg82Tbu6GB86EwfEXqyX2yly_I952qCNQK6uNpH1svUskry_GHLJhsejMON93sRA3SlrhZgyC2hE5vRoCNbtTZqIE1uSfG7xPaKNw6bDvVFxW1Z9H4tcmkYm4P7ZKl8z3lGTFP6OBn9zKDO76KUi0TCXfqBn7rMCFP5bz--sxrOWLF8OoXbrVR2mLfFFwGjKBSpK_TZdG-Ulb1n3S4llUjtbiosJLcLD_DhEjF1Fm1xZ3ADqzxNABT8vzTDbQqPat1uTeCbfIygghAsVaE-4' // The refresh token of your app user
-//       });
-//       let res = await sellingPartner.callAPI({
-//         operation:'listCatalogItems',
-//         query: {
-//             MarketplaceId: 'ATVPDKIKX0DER',
-//             ISBN:'9781568811024'
-//         }
-//       });
-//       console.log(JSON.stringify(res));
-//     } catch(e){
-//       console.log(e);
-//     }
-//   })();
+    //Get catalog item
+    let books = await sellingPartner.callAPI({
+        operation: 'listCatalogItems',
+        query: {
+            MarketplaceId: process.env.MARKET_ID,
+            ISBN: ISBN
+        }
+    });
 
-//   (async() => {
-//     try {
-//       let sellingPartner = new SellingPartnerAPI({
-//         region:'na', // The region of the selling partner API endpoint ("eu", "na" or "fe")
-//         refresh_token:'Atzr|IwEBIC4AJDFKXvRUub4yuIarsXbxYdBhZNCBWrr3hIOBoOlUcHvOg82Tbu6GB86EwfEXqyX2yly_I952qCNQK6uNpH1svUskry_GHLJhsejMON93sRA3SlrhZgyC2hE5vRoCNbtTZqIE1uSfG7xPaKNw6bDvVFxW1Z9H4tcmkYm4P7ZKl8z3lGTFP6OBn9zKDO76KUi0TCXfqBn7rMCFP5bz--sxrOWLF8OoXbrVR2mLfFFwGjKBSpK_TZdG-Ulb1n3S4llUjtbiosJLcLD_DhEjF1Fm1xZ3ADqzxNABT8vzTDbQqPat1uTeCbfIygghAsVaE-4' // The refresh token of your app user
-//       });
-//       let res = await sellingPartner.callAPI({
-//         operation:'getCompetitivePricing',
-//         query: {
-//             MarketplaceId: 'ATVPDKIKX0DER',
-//             Asins: ['1568811020'],
-//             ItemType: 'Asin'
-//         }
-//       });
-//       console.log(JSON.stringify(res));
-//     } catch(e){
-//       console.log(e);
-//     }
-//   })();
+    if (books.Items.length > 0) {
+        var ASIN = '';
+
+        //prompt for choice of right title
+
+        if (books.Items.length == 1) {
+            ASIN = books.Items[0].Identifiers.MarketplaceASIN.ASIN;
+        } else {
+            playSound('attn.mp3');
+            for (let i = 0; i < books.Items.length; i++) {
+                console.log(`[${i}] -> ${books.Items[i].AttributeSets[0].Title}`);
+            }
+            const choice = prompt('Which title is correct? >> ',function (val) {
+                if (val <= books.Items.length && val >= 0) return true
+                console.log('Please pick a number from the list...')
+               });
+            
+            ASIN = books.Items[choice].Identifiers.MarketplaceASIN.ASIN;
+        }
+
+        console.log(`ASIN: ${ASIN}`);
+
+        let pricing = await sellingPartner.callAPI({
+            operation: 'getCompetitivePricing',
+            query: {
+                MarketplaceId: process.env.MARKET_ID,
+                Asins: [ASIN],
+                ItemType: 'Asin'
+            }
+        });
+
+        var allPrices = [];
+        pricing[0].Product.CompetitivePricing.CompetitivePrices.forEach(price => {
+            if (price.Price.condition == 'Used') {
+                allPrices.push(price.Price.LandedPrice.Amount);
+            }
+        });
+        console.log(allPrices);
+        var bestPrice;
+        if (allPrices.length > 0) {
+            bestPrice = Math.max(...allPrices);
+        } else {
+            console.log('No price data to compare.');
+            bestPrice = 25;
+        }
+        console.log(`Best price: ${bestPrice}`);
+
+        let fees = await sellingPartner.callAPI({
+            operation: 'getMyFeesEstimateForASIN',
+            path: {
+                Asin: ASIN
+            },
+            body: {
+                FeesEstimateRequest: {
+                    MarketplaceId: process.env.MARKET_ID,
+                    IsAmazonFulfilled: true,
+                    PriceToEstimateFees: {
+                        ListingPrice: {
+                            CurrencyCode: 'USD',
+                            Amount: bestPrice
+                        }
+                    },
+                    Identifier: 'ggdfgsdggg'
+                }
+            }
+        });
+
+        console.log(fees);
+
+        console.log(`Total fees: ${fees.FeesEstimateResult.FeesEstimate.TotalFeesEstimate.Amount}`);
+
+        var priceDifference = bestPrice - fees.FeesEstimateResult.FeesEstimate.TotalFeesEstimate.Amount;
+        console.log(`Price difference: ${priceDifference}`);
+
+        if (priceDifference > process.env.PRICE_THRESHOLD) {
+            //create listing
+
+            playSound('success.mp3');
+        } else {
+            playSound('fail.mp3')
+        }
+
+    } else {
+        player.play('assets/fail.mp3', function (err) {
+            if (err) console.log(`Could not play audio: ${err}`)
+        })
+        console.warn('No item found!');
+
+    }
+
+    main();
+}
+
+function playSound(file) {
+    player.play(`assets/${file}`, function (err) {
+        if (err) console.log(`Could not play audio: ${err}`)
+    })
+}
+
+main();
