56a32e606dffc855324f574a5c6ac3a8ef66a96d
Owen Schwartz <oschwartz10612@gmail.com>
2021-04-10 18:14:35 -0400

    mysql

--- app.js
+++ app.js
@@ -1,257 +1,313 @@
-require('dotenv').config()
-const SellingPartnerAPI = require('amazon-sp-api');
-const prompt = require('prompt-validate');
-var player = require('play-sound')(opts = {});
-const ISBNAuditer = require('isbn3');
-const fs = require('fs');
+require("dotenv").config();
+const SellingPartnerAPI = require("amazon-sp-api");
+const prompt = require("prompt-validate");
+var player = require("play-sound")((opts = {}));
+const ISBNAuditer = require("isbn3");
+const util = require("util");
+const mysql = require("mysql");
 
 let sellingPartner = new SellingPartnerAPI({
-    region: 'na', // The region of the selling partner API endpoint ("eu", "na" or "fe")
-    refresh_token: process.env.REFRESH_TOKEN // The refresh token of your app user
+  region: "na", // The region of the selling partner API endpoint ("eu", "na" or "fe")
+  refresh_token: process.env.REFRESH_TOKEN, // The refresh token of your app user
 });
 
-const outFile = prompt('Out filename >> ');
-const skuFrag = prompt('SKU starting fragment >> ');
-var skuCounter = 0;
+var lastId = 0;
+var BOX = "";
 
-//TODO: Create and track sku - database?
-//TODO: Get sales rank and last 30 day sales?
+function makeDb(config) {
+  const connection = mysql.createConnection(config);
+  return {
+    query(sql, args) {
+      return util.promisify(connection.query).call(connection, sql, args);
+    },
+    close() {
+      return util.promisify(connection.end).call(connection);
+    },
+  };
+}
+
+const db = makeDb({
+  host: process.env.MYSQL_DOMAIN,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DATABASE,
+});
+
+async function getStartingSKU() {
+  const lastRow = await db.query(
+    "SELECT * FROM profitable_books ORDER BY id DESC LIMIT 0, 1"
+  );
+  BOX = lastRow[0].box_id;
+  lastId = lastRow[0].id;
+  main();
+}
+
+function getISBN() {
+    const ISBN = prompt("ISBN >> ", function (val) {
+        if (val == "stop") {
+          process.exit();
+        } else if (val.startsWith("box")) {
+            BOX = val;
+            console.log(`Nex box is is: ${BOX}`);
+            return true;
+        }
+        else if (
+          val != undefined &&
+          val != "" &&
+          ISBNAuditer.audit(val).validIsbn == true
+        ) {
+            return true;
+        }
+          
+        console.log("Please enter a valid ISBN...");
+        playSound("fail.mp3");
+      });
+      if (ISBN.startsWith("box")) {
+        return getISBN();
+      } else {
+        return ISBN;
+      }
+}
 
 async function main() {
-    var ASIN = '';
-    var title = '';
-    var bestPrice = 0;
+  var ASIN = "";
+  var title = "";
+  var rank = 0;
+  var bestPrice = 0;
+  const ISBN = getISBN();
 
-    const ISBN = prompt('ISBN >> ', function (val) {
-        if (val == 'stop') {
-            process.exit();
-        } else if (val.startsWith('boxid')) {
-            
+  //Get catalog item
+  let books = await sellingPartner.callAPI({
+    operation: "listCatalogItems",
+    query: {
+      MarketplaceId: process.env.MARKET_ID,
+      ISBN: ISBN,
+    },
+  });
+
+  if (books.Items.length > 0) {
+    if (books.Items.length == 1) {
+      ASIN = books.Items[0].Identifiers.MarketplaceASIN.ASIN;
+      title = books.Items[0].AttributeSets[0].Title;
+        rank = books.Items[0].SalesRankings[0].Rank;
+      var bestPricingData = await getCompetitivePricing([ASIN]);
+      bestPrice = bestPricingData.bestPrice;
+    } else {
+      allTitles = [];
+      books.Items.forEach((book) => {
+        allTitles.push(book.AttributeSets[0].Title);
+      });
+      const uniqueTitles = Array.from(new Set(allTitles));
+
+      console.log(uniqueTitles);
+
+      if (uniqueTitles.length > 1) {
+        //Book titles are different
+        playSound("attn.mp3");
+
+        for (let i = 0; i < books.Items.length; i++) {
+          console.log(`[${i}] -> ${books.Items[i].AttributeSets[0].Title}`);
         }
-        if (val != undefined && val != '' && ISBNAuditer.audit(val).validIsbn == true) return true;
-        console.log('Please enter a valid ISBN...')
-        playSound('fail.mp3');
-    });
-
-    //Get catalog item
-    let books = await sellingPartner.callAPI({
-        operation: 'listCatalogItems',
-        query: {
-            MarketplaceId: process.env.MARKET_ID,
-            ISBN: ISBN
-        }
-    });
-
-    if (books.Items.length > 0) {
-
-        if (books.Items.length == 1) {
-            ASIN = books.Items[0].Identifiers.MarketplaceASIN.ASIN;
-            title = books.Items[0].AttributeSets[0].Title;
-            var bestPricingData = await getCompetitivePricing([ASIN]);
-            bestPrice = bestPricingData.bestPrice;
-        } else {
-            allTitles = [];
-            books.Items.forEach(book => {
-                allTitles.push(book.AttributeSets[0].Title);
-            });
-            const uniqueTitles = Array.from(new Set(allTitles))
-
-            console.log(uniqueTitles);
-
-            if (uniqueTitles.length > 1) {
-                //Book titles are different
-                playSound('attn.mp3');
-
-                for (let i = 0; i < books.Items.length; i++) {
-                    console.log(`[${i}] -> ${books.Items[i].AttributeSets[0].Title}`);
-                }
-                const choice = prompt('Which title is correct? >> ', function (val) {
-                    if (val <= books.Items.length && val >= 0) return true
-                    console.log('Please pick a number from the list...')
-                });
-                ASIN = books.Items[choice].Identifiers.MarketplaceASIN.ASIN;
-                title = books.Items[choice].AttributeSets[0].Title;
-                var bestPricingData = await getCompetitivePricing([ASIN]);
-                bestPrice = bestPricingData.bestPrice;
-
-            } else {
-                //Book titles are the same. Find the one with the pricing. 
-                allPrices = [];
-                allASINs = [];
-                books.Items.forEach(book => {
-                    allASINs.push(book.Identifiers.MarketplaceASIN.ASIN);
-                });
-
-                var bestPricingData = await getCompetitivePricing(allASINs);
-                ASIN = bestPricingData.ASIN;
-                bestPrice = bestPricingData.bestPrice;
-                books.Items.forEach(book => {
-                    if (book.Identifiers.MarketplaceASIN.ASIN == ASIN) {
-                        title = book.AttributeSets[0].Title;
-                    }
-                });
-            }
-
-        }
-
-        console.log(`ASIN: ${ASIN}`);
-        console.log(`Best price: ${bestPrice}`);
-
-        let fees = await sellingPartner.callAPI({
-            operation: 'getMyFeesEstimateForASIN',
-            path: {
-                Asin: ASIN
-            },
-            body: {
-                FeesEstimateRequest: {
-                    MarketplaceId: process.env.MARKET_ID,
-                    IsAmazonFulfilled: true,
-                    PriceToEstimateFees: {
-                        ListingPrice: {
-                            CurrencyCode: 'USD',
-                            Amount: bestPrice
-                        }
-                    },
-                    Identifier: `${skuFrag}-${skuCounter}-fee`
-                }
-            }
+        const choice = prompt("Which title is correct? >> ", function (val) {
+          if (val <= books.Items.length && val >= 0) return true;
+          console.log("Please pick a number from the list...");
+        });
+        ASIN = books.Items[choice].Identifiers.MarketplaceASIN.ASIN;
+        title = books.Items[choice].AttributeSets[0].Title;
+        rank = books.Items[choice].SalesRankings[0].Rank
+        var bestPricingData = await getCompetitivePricing([ASIN]);
+        bestPrice = bestPricingData.bestPrice;
+      } else {
+        //Book titles are the same. Find the one with the pricing.
+        allPrices = [];
+        allASINs = [];
+        books.Items.forEach((book) => {
+          allASINs.push(book.Identifiers.MarketplaceASIN.ASIN);
         });
 
-        var fee = 0;
-
-        if (typeof fee.Error == undefined) {
-            console.error('Unable to find fee...');
-            fee = bestPrice * .5;
-        } else {
-            fee = fees.FeesEstimateResult.FeesEstimate.TotalFeesEstimate.Amount;
-        }
-
-        console.log(`Total fees: ${fee}`);
-
-        const priceDifference = bestPrice - fee;
-        console.log(`Price difference: ${priceDifference}`);
-
-        if (priceDifference > process.env.PRICE_THRESHOLD) {
-
-            playSound('success.mp3');
-
-            const condition = prompt('What is the condition? >> ');
-
-            fs.appendFileSync(outFile, `${ISBN},bookloader,${skuFrag}-${skuCounter},${ASIN},ASIN,${title},,,,,,,,${bestPrice},,,AMAZON_NA,,,,,,,,,,,,,,,,,,,${condition}\n`);
-            skuCounter += 1;
-
-        } else {
-            playSound('fail.mp3')
-        }
-
-    } else {
-        player.play('assets/fail.mp3', function (err) {
-            if (err) console.log(`Could not play audio: ${err}`)
-        })
-        console.warn('No item found!');
-
+        var bestPricingData = await getCompetitivePricing(allASINs);
+        ASIN = bestPricingData.ASIN;
+        bestPrice = bestPricingData.bestPrice;
+        books.Items.forEach((book) => {
+          if (book.Identifiers.MarketplaceASIN.ASIN == ASIN) {
+            title = book.AttributeSets[0].Title;
+            rank = book.SalesRankings[0].Rank
+          }
+        });
+      }
     }
 
-    main();
+    console.log(`ASIN: ${ASIN}`);
+    console.log(`Best price: ${bestPrice}`);
+
+    let fees = await sellingPartner.callAPI({
+      operation: "getMyFeesEstimateForASIN",
+      path: {
+        Asin: ASIN,
+      },
+      body: {
+        FeesEstimateRequest: {
+          MarketplaceId: process.env.MARKET_ID,
+          IsAmazonFulfilled: true,
+          PriceToEstimateFees: {
+            ListingPrice: {
+              CurrencyCode: "USD",
+              Amount: bestPrice,
+            },
+          },
+          Identifier: `${BOX}_rb${lastId + 1}-fee`,
+        },
+      },
+    });
+
+    var fee = 0;
+
+    if (typeof fee.Error == undefined) {
+      console.error("Unable to find fee...");
+      fee = bestPrice * 0.5;
+    } else {
+      fee = fees.FeesEstimateResult.FeesEstimate.TotalFeesEstimate.Amount;
+    }
+
+    console.log(`Total fees: ${fee}`);
+
+    const priceDifference = bestPrice - fee;
+    console.log(`Price difference: ${priceDifference}`);
+
+    if (priceDifference > process.env.PRICE_THRESHOLD) {
+      playSound("success.mp3");
+
+      const condition = prompt("What is the condition? >> ");
+
+      const insertedRow = await db.query("INSERT INTO profitable_books SET ?", {
+        SKU: `${BOX}_rb${lastId + 1}`,
+        box_id: BOX,
+        ISBN: ISBN,
+        ASIN: ASIN,
+        title: title,
+        condition: condition,
+        profit: priceDifference,
+        best_price: bestPrice,
+        fee: fee,
+        rank: rank,
+      });
+      lastId = insertedRow.insertId;
+
+    } else {
+      playSound("fail.mp3");
+    }
+  } else {
+    player.play("assets/fail.mp3", function (err) {
+      if (err) console.log(`Could not play audio: ${err}`);
+    });
+    console.warn("No item found!");
+  }
+
+  main();
 }
 
 function playSound(file) {
-    player.play(`assets/${file}`, function (err) {
-        if (err) console.log(`Could not play audio: ${err}`)
-    })
+  player.play(`assets/${file}`, function (err) {
+    if (err) console.log(`Could not play audio: ${err}`);
+  });
 }
 
-const median = arr => {
-    const mid = Math.floor(arr.length / 2),
-      nums = [...arr].sort((a, b) => a - b);
-    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-  };
+const median = (arr) => {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b);
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
 
 async function getCompetitivePricing(ASINS) {
-    let pricing = await sellingPartner.callAPI({
-        operation: 'getCompetitivePricing',
-        query: {
-            MarketplaceId: process.env.MARKET_ID,
-            Asins: ASINS,
-            ItemType: 'Asin'
-        }
-    });
+  let pricing = await sellingPartner.callAPI({
+    operation: "getCompetitivePricing",
+    query: {
+      MarketplaceId: process.env.MARKET_ID,
+      Asins: ASINS,
+      ItemType: "Asin",
+    },
+  });
 
-    var allPrices = [];
-    var validASINs = [];
-    console.log(JSON.stringify(pricing, null, 2));
-    pricing.forEach(product => {
-        product.Product.CompetitivePricing.CompetitivePrices.forEach(price => {
-            if (price.Price.condition == 'Used') {
-                allPrices.push(price.Price.LandedPrice.Amount);
-                if (!validASINs.includes(product.ASIN)) {
-                    validASINs.push(product.ASIN);
-                }
-            }
-        });
+  var allPrices = [];
+  var validASINs = [];
+  pricing.forEach((product) => {
+    product.Product.CompetitivePricing.CompetitivePrices.forEach((price) => {
+      if (price.Price.condition == "Used") {
+        allPrices.push(price.Price.LandedPrice.Amount);
+        if (!validASINs.includes(product.ASIN)) {
+          validASINs.push(product.ASIN);
+        }
+      }
+    });
+  });
+
+  if (allPrices.length == 0) {
+    console.error("No pricing data available for used products, trying new...");
+
+    pricing.forEach((product) => {
+      product.Product.CompetitivePricing.CompetitivePrices.forEach((price) => {
+        if (price.Price.condition == "New") {
+          allPrices.push(price.Price.LandedPrice.Amount);
+          if (!validASINs.includes(product.ASIN)) {
+            validASINs.push(product.ASIN);
+          }
+        }
+      });
     });
 
     if (allPrices.length == 0) {
-        console.error('No pricing data available for used products, trying new...');
+      console.error("No pricing data available, save for later?");
+      playSound("attn.mp3");
+      main();
+    }
+  }
 
-        pricing.forEach(product => {
-            product.Product.CompetitivePricing.CompetitivePrices.forEach(price => {
-                if (price.Price.condition == 'New') {
-                    allPrices.push(price.Price.LandedPrice.Amount);
-                    if (!validASINs.includes(product.ASIN)) {
-                        validASINs.push(product.ASIN);
-                    }
-                }
-            });
-        });
-
-        if (allPrices.length == 0) {
-            console.error('No pricing data available, save for later?');
-            playSound('attn.mp3');
-            main();
-        }
+  var correctASIN;
+  if (validASINs.length > 1) {
+    playSound("attn.mp3");
+    for (let i = 0; i < validASINs.length; i++) {
+      console.log(`[${i}] -> ${validASINs[i]}`);
     }
 
-    var correctASIN;
-    if (validASINs.length > 1) {
-        playSound('attn.mp3');
-        for (let i = 0; i < validASINs.length; i++) {
-            console.log(`[${i}] -> ${validASINs[i]}`);
-        }
+    const choice = prompt(
+      "Multiple ASINs! Which is correct? >> ",
+      function (val) {
+        if (val <= validASINs.length && val >= 0) return true;
+        console.log("Please pick a number from the list...");
+      }
+    );
 
-        const choice = prompt('Multiple ASINs! Which is correct? >> ', function (val) {
-            if (val <= validASINs.length && val >= 0) return true
-            console.log('Please pick a number from the list...')
-        });
+    correctASIN = validASINs[choice];
+  } else {
+    correctASIN = validASINs[0];
+  }
 
-        correctASIN = validASINs[choice];
-    } else {
-        correctASIN = validASINs[0];
-    }
+  //var bestPrice = Math.min(...allPrices);
+  console.log(allPrices);
+  console.log(Math.round(allPrices.length * 0.3));
+  var i =
+    Math.round(allPrices.length * 0.3) == 0
+      ? 1
+      : Math.round(allPrices.length * 0.3);
+  console.log(i);
+  var total = 0;
+  for (let a = 0; a < i; a++) {
+    console.log(allPrices[a]);
+    total += allPrices[a];
+  }
+  console.log(total);
+  const bestPrice = total / i;
+  console.log(bestPrice);
 
-    //var bestPrice = Math.min(...allPrices);
-    console.log(allPrices);
-    console.log(Math.round(allPrices.length*.3));
-    var i = ((Math.round(allPrices.length*.3) == 0) ? 1 : Math.round(allPrices.length*.3))
-    console.log(i);
-    var total = 0;
-    for (let a = 0; a < i; a++) {
-        console.log(allPrices[a]);
-        total += allPrices[a];
-    }
-    console.log(total);
-    const bestPrice = total/i;
-    console.log(bestPrice);
+  if (bestPrice == Infinity) {
+    bestPrice = 0;
+  }
 
-    if (bestPrice == Infinity) {
-        bestPrice = 0;
-    }
+  return {
+    ASIN: correctASIN,
 
-    return {
-        ASIN: correctASIN,
-
-        bestPrice: bestPrice
-    }
+    bestPrice: bestPrice,
+  };
 }
 
-main();
+getStartingSKU();
