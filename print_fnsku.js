f3dd279e77bd3bd8f6f1bec1ff1be9babcff9260
Owen Schwartz <oschwartz10612@gmail.com>
2021-04-25 13:10:31 -0400

    Print labes

--- print_fnsku.js
+++ print_fnsku.js
@@ -6,9 +6,9 @@ const makeDb = require("./lib/db");
 const { printTable } = require('console-table-printer');
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
-const doc = new PDFDocument();
 const ptp = require("pdf-to-printer");
 const path = require('path');
+const delay = require('delay');
 
 let sellingPartner = new SellingPartnerAPI({
     region: "na", // The region of the selling partner API endpoint ("eu", "na" or "fe")
@@ -23,6 +23,8 @@ const db = makeDb({
 });
 
 toPrint = [];
+var index = parseInt(prompt('INDEX (start at 1) >> '));
+var page = parseInt(prompt('Next page (start at 0) >> '));
 
 async function main() {
     const ISBN = prompt('ISBN >> ');
@@ -33,7 +35,7 @@ async function main() {
         if (data.length > 1) {
             playSound('fail.mp3');
             console.log('More than 1 row!');
-            main();
+            return main();
         } else if (data.length == 0) {
             playSound('fail.mp3');
             console.log('Not known FNSKU...');
@@ -50,26 +52,61 @@ async function main() {
                 toPrint = [];
             } else {
                 toPrint.push(data[0].FNSKU);
+                return main();
             }
-
-
         }
-
-        main();
     }
 }
 
 main();
 
 async function printPage() {
-    doc.pipe(fs.createWriteStream('./assets/output.pdf'));
 
-    doc.font('./assets/LibreBarcode128Text-Regular.ttf').fontSize(40).text(toPrint[0], 104, 72);
-    doc.font('./assets/LibreBarcode128Text-Regular.ttf').fontSize(40).text(toPrint[1], 304, 72);
-    doc.font('./assets/LibreBarcode128Text-Regular.ttf').fontSize(40).text(toPrint[2], 504, 72);
+    if (page == 4) {
+        playSound('attn.mp3');
+        index += 1;
+        prompt('Reload labels...');
+        page = 0;
+    }
 
+    const id = makeid(5);
+    const doc = new PDFDocument();
+    var pdfStream = fs.createWriteStream(path.join(__dirname, `/output/output${id}.pdf`));
+
+    doc.font('./assets/LibreBarcode128Text-Regular.ttf').fontSize(44);
+
+    doc.text(toPrint[0], 104-(doc.widthOfString(toPrint[0])/2), (72-(doc.heightOfString(toPrint[0])/2))*index, {
+        width: doc.page.width
+    });
+    doc.text(toPrint[1], 304-(doc.widthOfString(toPrint[1])/2), (72-(doc.heightOfString(toPrint[1])/2))*index,  {
+        width: doc.page.width
+    });
+    doc.text(toPrint[2], 504-(doc.widthOfString(toPrint[2])/2), (72-(doc.heightOfString(toPrint[2])/2))*index,  {
+        width: doc.page.width
+    });
+
+    doc.pipe(pdfStream)
     doc.end();
 
-    await ptp.print(path.join(__dirname, '/assets/output.pdf'), { printer: 'hp_LaserJet_4200' });
+    playSound('attn.mp3');
 
+    console.log(`Waiting for pdf page ${page}`);
+    pdfStream.addListener('finish', () => {
+        console.log(`Printing pdf page ${page}`);
+        await ptp.print(path.join(__dirname, `/output/output${id}.pdf`), { printer: 'hp_LaserJet_4200' });
+        main();
+    });
+
+    page += 1;
+}
+
+function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
 }
