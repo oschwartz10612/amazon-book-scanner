6b6a84366df5d0bc03b23cfe09762fed4116db5e
Owen Schwartz <oschwartz10612@gmail.com>
2021-05-02 15:19:10 -0400

    Fix print

--- functions/print_fnsku.js
+++ functions/print_fnsku.js
@@ -6,7 +6,8 @@ const makeDb = require("../lib/db");
 const { printTable } = require('console-table-printer');
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
-const ptp = require("pdf-to-printer");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const path = require('path');
 const delay = require('delay');
 
@@ -14,6 +15,8 @@ let sellingPartner = new SellingPartnerAPI({
     region: "na", // The region of the selling partner API endpoint ("eu", "na" or "fe")
     refresh_token: process.env.REFRESH_TOKEN, // The refresh token of your app user
 });
+
+
 
 const db = makeDb({
     host: process.env.MYSQL_DOMAIN,
@@ -31,7 +34,7 @@ async function main() {
     if (ISBN == 'stop') {
         process.exit(0);
     } else {
-        const data = await db.query('SELECT id, title, FNSKU from profitable_books WHERE ISBN = ?', [ISBN]);
+        const data = await db.query('SELECT id, title, FNSKU from profitable_books WHERE ISBN = ? AND FNSKU IS NOT NULL', [ISBN]);
         if (data.length > 1) {
             playSound('fail.mp3');
             console.log('More than 1 row!');
@@ -40,7 +43,7 @@ async function main() {
             playSound('fail.mp3');
             console.log('Not known FNSKU...');
             return main();
-            
+
         } else {
 
             printTable(data);
@@ -49,7 +52,9 @@ async function main() {
             if (toPrint.length == 2) {
                 toPrint.push(data[0].FNSKU);
                 await printPage();
+                page += 1;
                 toPrint = [];
+                main();
             } else {
                 toPrint.push(data[0].FNSKU);
                 return main();
@@ -62,7 +67,7 @@ main();
 
 async function printPage() {
 
-    if (page == 4) {
+    if (page == 3) {
         playSound('attn.mp3');
         index += 1;
         prompt('Reload labels...');
@@ -72,16 +77,17 @@ async function printPage() {
     const id = makeid(5);
     const doc = new PDFDocument();
     var pdfStream = fs.createWriteStream(path.join(__dirname, `../output/output${id}.pdf`));
+    const reverseToPrint = toPrint.reverse();
 
     doc.font('./assets/LibreBarcode128Text-Regular.ttf').fontSize(44);
 
-    doc.text(toPrint[0], 104-(doc.widthOfString(toPrint[0])/2), (72-(doc.heightOfString(toPrint[0])/2))*index, {
+    doc.text(reverseToPrint[0], 104 - (doc.widthOfString(reverseToPrint[0]) / 2), (72 - (doc.heightOfString(reverseToPrint[0]) / 2)) * (index*2), {
         width: doc.page.width
     });
-    doc.text(toPrint[1], 304-(doc.widthOfString(toPrint[1])/2), (72-(doc.heightOfString(toPrint[1])/2))*index,  {
+    doc.text(reverseToPrint[1], 304 - (doc.widthOfString(reverseToPrint[1]) / 2), (72 - (doc.heightOfString(reverseToPrint[1]) / 2)) * (index*2), {
         width: doc.page.width
     });
-    doc.text(toPrint[2], 504-(doc.widthOfString(toPrint[2])/2), (72-(doc.heightOfString(toPrint[2])/2))*index,  {
+    doc.text(reverseToPrint[2], 504 - (doc.widthOfString(reverseToPrint[2]) / 2), (72 - (doc.heightOfString(reverseToPrint[2]) / 2)) * (index*2), {
         width: doc.page.width
     });
 
@@ -91,22 +97,29 @@ async function printPage() {
     playSound('attn.mp3');
 
     console.log(`Waiting for pdf page ${page}`);
-    pdfStream.addListener('finish', () => {
-        console.log(`Printing pdf page ${page}`);
-        await ptp.print(path.join(__dirname, `../output/output${id}.pdf`), { printer: 'hp_LaserJet_4200' });
-        main();
-    });
+    return new Promise((resolve, reject) => {
+        pdfStream.addListener('finish', async () => {
+            console.log(`Printing pdf page ${page}`);
+            const filepath = path.join(__dirname, `../output/output${id}.pdf`);
+            const { stdout, stderr } = await exec(`lp ${filepath}`);
 
-    page += 1;
-}
+            if (stderr) {
+                console.error(`error: ${stderr}`);
+                reject();
+            }
+            console.log(`Number of files ${stdout}`);
+            resolve();
+        });
+    });
+};
 
 function makeid(length) {
-    var result           = [];
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = [];
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result.push(characters.charAt(Math.floor(Math.random() * 
- charactersLength)));
-   }
-   return result.join('');
+    for (var i = 0; i < length; i++) {
+        result.push(characters.charAt(Math.floor(Math.random() *
+            charactersLength)));
+    }
+    return result.join('');
 }
