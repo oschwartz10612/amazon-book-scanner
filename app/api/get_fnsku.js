95f6df62c0c4f961903bec9fd51ef227ec8f0889
Owen Schwartz <oschwartz10612@gmail.com>
2021-05-15 16:39:36 -0400

    Dont search databse if there are no fnsky to update

--- app/api/get_fnsku.js
+++ app/api/get_fnsku.js
@@ -3,39 +3,48 @@ const db = require("./db");
 const sellingPartner = require("./sp");
 
 async function getFNSKU(socket) {
+  const data = await db.query(
+    "SELECT * FROM profitable_books WHERE FNSKU IS NULL AND sent = 1 LIMIT 50"
+  );
+  const SKUs = data.map((book) => book.SKU);
+  console.log(SKUs.length);
+  socket.emit("logs", `Items to update from database: ${SKUs.length}`);
 
-    const data = await db.query('SELECT * FROM profitable_books WHERE FNSKU IS NULL AND sent = 1 LIMIT 50');
-    const SKUs = data.map(book => book.SKU);
-    console.log(SKUs.length);
-    socket.emit('logs', SKUs.length);
-
+  if (SKUs.length > 0) {
     var count = 0;
 
     const summary = await sellingPartner.callAPI({
-        operation: "getInventorySummaries",
-        query: {
-            marketplaceIds: [process.env.MARKET_ID],
-            granularityId: process.env.MARKET_ID,
-            granularityType: 'Marketplace',
-            sellerSkus: SKUs
-        }
+      operation: "getInventorySummaries",
+      query: {
+        marketplaceIds: [process.env.MARKET_ID],
+        granularityId: process.env.MARKET_ID,
+        granularityType: "Marketplace",
+        sellerSkus: SKUs,
+      },
     });
-
+    
     console.log(summary.inventorySummaries.length);
-    socket.emit('logs', summary.inventorySummaries.length);
+    socket.emit("logs", summary.inventorySummaries.length);
 
-    summary.inventorySummaries.forEach(async item => {
-        try {
-            await db.query('UPDATE profitable_books SET FNSKU = ? WHERE SKU = ?', [item.fnSku, item.sellerSku]);
-        } catch (error) {
-            console.log(`There was a problem setting sql for ${item.sellerSku}`);
-        }
-        count += 1;
-        console.log(`${count} - ${item.sellerSku}`);
-        socket.emit('logs', `${count} - ${item.sellerSku}`);
+    summary.inventorySummaries.forEach(async (item) => {
+      try {
+        await db.query("UPDATE profitable_books SET FNSKU = ? WHERE SKU = ?", [
+          item.fnSku,
+          item.sellerSku,
+        ]);
+      } catch (error) {
+        console.log(`There was a problem setting sql for ${item.sellerSku}`);
+      }
+      count += 1;
+      console.log(`${count} - ${item.sellerSku}`);
+      socket.emit("logs", `${count} - ${item.sellerSku}`);
     });
-};
+  } else {
+    socket.emit("logs", "Nothing to update...");
+  }
+
+}
 
 module.exports = {
-    getFNSKU: getFNSKU
+  getFNSKU: getFNSKU,
 };
